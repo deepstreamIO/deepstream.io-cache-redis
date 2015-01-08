e60fe0028f4b896120059d7e63e8cc19bcfc4f06
hoxton_one <info@golden-layout.com>
2015-01-08 16:03:34 +0000

    initial separation

---
+++ src/cache-connector.js
@@ -0,0 +1,86 @@
+var Connection = require( './connection' ),
+	util = require( 'util' );
+
+/**
+ * A [deepstream](http://deepstream.io) cache connector
+ * for [Redis](http://redis.io)
+ *
+ * Since Redis, on top of caching key/value combinations in
+ * memory, writes them to disk it can make a storage connector
+ * obsolete
+ *
+ * @author Wolfram Hempel
+ * @copyright 2015 Hoxton One Ltd.
+ *
+ * @param {Object} options redis connection options. Please see ./connection.js
+ *                         for details
+ *
+ * @constructor
+ */
+var CacheConnector = function( options ) {
+	Connection.call( this, options );
+
+	this.name = 'deepstream-cache-redis';
+	this.version = '0.2.2';
+};
+
+util.inherits( CacheConnector, Connection );
+
+/**
+ * Deletes an entry from the cache.
+ *
+ * @param   {String}   key
+ * @param   {Function} callback Will be called with null for successful deletions or with an error message
+ *
+ * @private
+ * @returns {void}
+ */
+CacheConnector.prototype.delete = function( key, callback ) {
+	this._client.del( key, callback );
+};
+
+/**
+ * Writes a value to the cache.
+ *
+ * @param {String}   key
+ * @param {Object}   value
+ * @param {Function} callback Will be called with null for successful set operations or with an error message string
+ *
+ * @private
+ * @returns {void}
+ */
+CacheConnector.prototype.set = function( key, value, callback ) {
+	this._client.set( key, JSON.stringify( value ), callback );
+};
+
+/**
+ * Retrieves a value from the cache
+ *
+ * @param {String}   key
+ * @param {Function} callback Will be called with null and the originally stored object
+ *                            for successful operations or with an error message string
+ *
+ * @private
+ * @returns {void}
+ */
+CacheConnector.prototype.get = function( key, callback ) {
+	this._client.get( key, function( error, result ){
+		var parsedResult;
+
+		if( result === null ) {
+			callback( error, null );
+			return;
+		}
+
+		try {
+			parsedResult = JSON.parse( result );
+		} catch ( e ) {
+			callback( e );
+			return;
+		}
+
+		callback( null, parsedResult );
+	});
+};
+
+module.exports = CacheConnector;
---
+++ src/connection.js
@@ -0,0 +1,104 @@
+var redis = require( 'redis' ),
+	EventEmitter = require( 'events' ).EventEmitter,
+	utils = require( 'util' );
+
+/**
+ * Generic connection to Redis. Can be extended or
+ * instantiated.
+ *
+ * @param {Object} options A map of connection parameters, namely
+ *
+ * {
+ * 		host: <Number>
+ * 		port: <String>
+ * 		[password]: <String> //optional
+ * }
+ *
+ * @constructor
+ */
+var Connection = function( options ) {
+	this.isReady = false;
+
+	this._validateOptions( options );
+	this._options = options;
+	
+	this.client = redis.createClient( options.port, options.host );
+	
+	if( options.password ) {
+		this.client.auth( options.password, this._onAuthResult.bind( this ) );
+	}
+	
+	this.client.on( 'ready', this._onReady.bind( this ) );
+	this.client.on( 'error', this._onError.bind( this ) );
+	this.client.on( 'end', this._onDisconnect.bind( this ) );
+};
+
+utils.inherits( Connection, EventEmitter );
+
+/**
+ * Callback for authentication responses
+ *
+ * @param   {String} error  Error message or null
+ *
+ * @void
+ * @returns {void}
+ */
+Connection.prototype._onAuthResult = function( error ) {
+	if( error ) {
+		this._onError( 'Failed to authenticate connection: ' + error.toString() );
+	}
+};
+
+/**
+ * Callback for established connections
+ *
+ * @ready
+ * @returns {void}
+ */
+Connection.prototype._onReady = function() {
+	this.isReady = true;
+	this.emit( 'ready' );
+};
+
+/**
+ * Generic error callback
+ *
+ * @param   {String} error
+ *
+ * @ready
+ * @returns {void}
+ */
+Connection.prototype._onError = function( error ) {
+	this.emit( 'error', 'REDIS error:' + error );
+};
+
+/**
+ * Callback for disconnection events
+ *
+ * @param   {String} error reason for disconnect
+ *
+ * @ready
+ * @returns {void}
+ */
+Connection.prototype._onDisconnect = function( error ) {
+	this._onError( 'disconnected' );
+};
+
+/**
+ * Checks if all required parameters are present
+ *
+ * @param   {Object} options
+ *
+ * @ready
+ * @returns {void}
+ */
+Connection.prototype._validateOptions = function( options ) {
+	if( !options.host ) {
+		throw new Error( 'Missing option \'host\' for redis-connector' );
+	}
+	if( !options.port ) {
+		throw new Error( 'Missing option \'port\' for redis-connector' );
+	}
+};
+
+module.exports = Connection;
